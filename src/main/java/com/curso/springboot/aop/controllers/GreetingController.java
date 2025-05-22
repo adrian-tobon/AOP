2ca63eb606a80c13fa9c52d9219acbf43040d431
ctobonpe <ctobonpe@gmail.com>
2025-05-22 15:57:46 -0500

    first commit

---
+++ src/main/java/com/curso/springboot/aop/controllers/GreetingController.java
@@ -0,0 +1,21 @@
+package com.curso.springboot.aop.controllers;
+
+import java.util.Collections;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	
+	@GetMapping("/greeting")
+	
+	public ResponseEntity<?> greeting()
+	{
+		return ResponseEntity.ok(Collections.singletonMap("greeting", null));
+	}
+	
+
+}
